fb4e3dc0bfdaaec1a21446b3f04a5c385fb2cb40
Samira <grossisamira@hotmail.com>
2024-07-26 21:06:25 -0300

    feat: adds product class

---
+++ src/product/controllers/product.controller.ts
@@ -0,0 +1,67 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ProductService } from '../services/product.service';
+import { ProductEntity } from '../entities/product.entity';
+
+@Controller('/produtos')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<ProductEntity[]> {
+    return this.productService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<ProductEntity> {
+    return this.productService.findById(id);
+  }
+
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findByTitle(@Param('titulo') title: string): Promise<ProductEntity[]> {
+    return this.productService.findByTitle(title);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() product: ProductEntity): Promise<ProductEntity> {
+    return this.productService.create(product);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() product: ProductEntity): Promise<ProductEntity> {
+    return this.productService.update(product);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.delete(id);
+  }
+
+  @Get('/preco/:preco')
+  @HttpCode(HttpStatus.OK)
+  findHigherPrice(@Param('preco') price: number): Promise<ProductEntity[]> {
+    return this.productService.findHigherPrice(price);
+  }
+
+  @Get('/preco/:preco')
+  @HttpCode(HttpStatus.OK)
+  findLowerPrice(@Param('preco') price: number): Promise<ProductEntity[]> {
+    return this.productService.findLowerPrice(price);
+  }
+}
---
+++ src/product/services/product.service.ts
@@ -0,0 +1,122 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  DeleteResult,
+  ILike,
+  Repository,
+  MoreThanOrEqual,
+  LessThanOrEqual,
+} from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryService } from '../../category/services/category.service';
+import { ProductEntity } from '../entities/product.entity';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(ProductEntity)
+    private productRepository: Repository<ProductEntity>,
+    private categoryService: CategoryService,
+  ) {}
+
+  async findAll(): Promise<ProductEntity[]> {
+    return await this.productRepository.find({
+      relations: {
+        category: true,
+      },
+    });
+  }
+
+  async findById(id: number): Promise<ProductEntity> {
+    let product = await this.productRepository.findOne({
+      where: { id },
+      relations: { category: true },
+    });
+
+    if (!product)
+      throw new HttpException('Produto não encontrado!', HttpStatus.NOT_FOUND);
+
+    return product;
+  }
+
+  async findByTitle(title: string): Promise<ProductEntity[]> {
+    return await this.productRepository.find({
+      where: { title: ILike(`%${title}%`) },
+      relations: { category: true },
+    });
+  }
+
+  async create(product: ProductEntity): Promise<ProductEntity> {
+    if (product.category) {
+      let category = await this.categoryService.findById(product.category.id);
+      if (!category)
+        throw new HttpException(
+          'Categoria não foi encontrada!',
+          HttpStatus.NOT_FOUND,
+        );
+    }
+
+    return await this.productRepository.save(product);
+  }
+
+  async update(product: ProductEntity): Promise<ProductEntity> {
+    let id = await this.findById(product.id);
+
+    if (!id || !product.id)
+      throw new HttpException('Produto não encontrado!', HttpStatus.NOT_FOUND);
+    if (product.category) {
+      await this.categoryService.findById(product.category.id);
+      return await this.productRepository.save(product);
+    }
+
+    return await this.productRepository.save(product);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    let product = await this.findById(id);
+    if (!product)
+      throw new HttpException('Produto não encontrado!', HttpStatus.NOT_FOUND);
+    return await this.productRepository.delete(id);
+  }
+
+  async findHigherPrice(price: number): Promise<ProductEntity[]> {
+    if (price == 0) {
+      throw new HttpException(
+        'Preço deve ser maior que zero!',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const getProduct = (func) =>
+      this.productRepository.find({
+        where: { price: func(price) },
+        relations: {
+          category: true,
+        },
+      });
+
+    if (price > 500) {
+      return await getProduct(MoreThanOrEqual);
+    } else {
+      return await getProduct(LessThanOrEqual);
+    }
+  }
+
+  async findLowerPrice(price: number): Promise<ProductEntity[]> {
+    if (price > 500)
+      throw new HttpException(
+        'O preço deve ser menor ou igual a 500',
+        HttpStatus.NOT_FOUND,
+      );
+
+    const product = await this.productRepository.find({
+      where: { price: LessThanOrEqual(price) },
+      relations: {
+        category: true,
+      },
+      order: {
+        price: 'ASC',
+      },
+    });
+    return product;
+  }
+}
