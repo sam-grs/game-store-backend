3b98f2e5bacb213c7ee3a40be2eb5816eed51393
Samira <grossisamira@hotmail.com>
2024-07-26 21:07:55 -0300

    feat: adds category class

---
+++ src/category/category.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryEntity } from './entities/category.entity';
+import { CategoryService } from './services/category.service';
+import { CategoryController } from './controllers/category.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([CategoryEntity])],
+  providers: [CategoryService],
+  controllers: [CategoryController],
+  exports: [TypeOrmModule],
+})
+export class CategoryModule {}
---
+++ src/category/entities/category.entity.ts
@@ -0,0 +1,18 @@
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ProductEntity } from '../../product/entities/product.entity';
+
+@Entity({ name: 'tb_category' })
+export class CategoryEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Transform(({ value }: TransformFnParams) => value?.trim())
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  category_type: string;
+
+  @OneToMany(() => ProductEntity, (product) => product.category)
+  product: ProductEntity[];
+}
